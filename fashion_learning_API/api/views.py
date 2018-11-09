8a7f13233c3340f0602341a3eb030450eabf635a
Gian <gian.bravo@alumnos.usm.cl>
2018-11-09 12:21:45 -0300

    first commit with POST response on API

--- fashion_learning_API/api/views.py
+++ fashion_learning_API/api/views.py
@@ -14,11 +14,11 @@ def index(request):
 	# image = request.FILES["image"]
 
 	# Do predictions using the ML model
-	# prediction = make_predictions(image.temporary_file_path)
+	prediction = make_predictions(image.temporary_file_path)
 
 	# prediction = make_predictions(image.filename)
 
-	prediction = make_predictions("image.filename")
+	# prediction = make_predictions("image.filename")
 	
 	# Se crea respuesta
 	response = {"data": prediction}
